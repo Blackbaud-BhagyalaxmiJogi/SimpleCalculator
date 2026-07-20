b4c240e59fcea2ca7af43093bd3aee037318f6ba
Bhagyalaxmi Jogi <Bhagyalaxmi.Jogi@blackbaud.me>
2026-07-20 16:54:24 +0530

    added services in main file

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using Microsoft.Extensions.DependencyInjection;
 using SimpleCalculator.Calculation;
 using SimpleCalculator.Evaluation;
 using SimpleCalculator.Logging;
@@ -12,21 +13,39 @@ namespace SimpleCalculator
 
         static void Main(string[] args)
         {
-            var historyRepository = new HistoryRepository("expression_history.txt");
-            var evaluationService = new ExpressionEvaluationService(new ExpressionCalculator(), historyRepository);
+            ServiceProvider serviceProvider = ConfigureServices();
 
-            var menuController = new MenuController(
-                new ConsoleExpressionRunner(evaluationService),
-                new BatchFileProcessor(evaluationService),
-                new HistoryPresenter(historyRepository),
-                historyRepository);
-
-            var app = new CalculatorApp(menuController);
-
-
+            var app = serviceProvider.GetRequiredService<CalculatorApp>();
             app.Run();
         }
 
+        private static ServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
         
+            services.AddSingleton<Tokenizer>();
+            services.AddSingleton<ShuntingYardConverter>();
+            services.AddSingleton<PostfixEvaluator>();
+            services.AddSingleton<IExpressionCalculator, ExpressionCalculator>();
+
+            
+            services.AddSingleton(sp => new HistoryRepository("expression_history.txt"));
+            services.AddSingleton<IHistoryWriter>(sp => sp.GetRequiredService<HistoryRepository>());
+            services.AddSingleton<IHistoryReader>(sp => sp.GetRequiredService<HistoryRepository>());
+            services.AddSingleton<IHistoryManager>(sp => sp.GetRequiredService<HistoryRepository>());
+
+            
+            services.AddSingleton<ExpressionEvaluationService>();
+            services.AddSingleton<ConsoleExpressionRunner>();
+            services.AddSingleton<BatchFileProcessor>();
+            services.AddSingleton<HistoryPresenter>();
+            services.AddSingleton<MenuController>();
+            services.AddSingleton<CalculatorApp>();
+
+            return services.BuildServiceProvider();
+        }
+
+
     }
 }
